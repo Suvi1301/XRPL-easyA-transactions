5db652215e5606cd6b508625d709df9ae4bc8483
jTanG0506 <mailto.jtang@gmail.com>
2024-04-14 00:32:07 +0100

    chore: more formatting

--- SmartContract/contracts/ERC20.sol
+++ SmartContract/contracts/ERC20.sol
@@ -7,8 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract XRPayToken is ERC20 {
     uint8 private _decimals = 6;
 
-    constructor() ERC20("XRPayCoin", "XRPC") {
-    }
+    constructor() ERC20("XRPayCoin", "XRPC") {}
 
     function mint(uint256 _amount) public {
         _mint(msg.sender, _amount);
